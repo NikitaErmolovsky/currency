82f9bf001341f9fe626e29974a6d096182c6292c
Ermolovskii <ermolovskii.nikita@mail.ru>
2021-01-04 15:08:09 +0300

    init

---
+++ src/main/java/com/ermolovsky/currency/curclient/CurrencyModel.java
@@ -0,0 +1,53 @@
+package com.ermolovsky.currency.curclient;
+
+import java.sql.Timestamp;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CurrencyModel {
+private String disclaimer;
+private String license;
+private Timestamp timestamp;
+private String base;
+private TreeMap<String,Double> rates;
+
+    public String getDisclaimer() {
+        return disclaimer;
+    }
+
+    public void setDisclaimer(String disclaimer) {
+        this.disclaimer = disclaimer;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public TreeMap<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(TreeMap<String, Double> rates) {
+        this.rates = rates;
+    }
+}
